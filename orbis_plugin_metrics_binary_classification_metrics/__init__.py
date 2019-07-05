02ceb2091add6c06cf5561ef69f0703651fffdbc
fabod <2189668+fabod@users.noreply.github.com>
2019-07-05 14:39:32 +0200

    wip

--- orbis_plugin_metrics_binary_classification_metrics/__init__.py
+++ orbis_plugin_metrics_binary_classification_metrics/__init__.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+
+__name__ = "orbis_plugin_metrics_binary_classification_metrics"
+__version__ = "1.1.dev"
+__author__ = "fabod"
+__year__ = "2019"
+__description__ = "The binary_classification_metrics metrics plugin for Orbis"
+__license__ = "GPL2"
+__min_python_version__ = "3.6"
+__requirements_file__ = "requirements.txt"
+__url__ = "https://github.com/orbis-eval/orbis_plugin_metrics_binary_classification_metrics"
+__type__ = "plugin"
+
 from .main import BinaryClassificationMetrics as Main
